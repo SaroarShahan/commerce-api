9dd8801a03b4d8e0cc3ac8810f9c097b89bfa444
SaroarShahan <saroar.shahan@gmail.com>
2024-12-30 16:49:41 +0600

    refactor(authenticateUser): Update authenticateUser middleware

--- src/middleware/authenticateUser.js
+++ src/middleware/authenticateUser.js
@@ -1,16 +1,33 @@
 const catchAsync = require('./catchAsync');
 const { AppError } = require('../errors');
 const { isTokenValid } = require('../utils/jwt');
+const User = require('../db/models/user');
 
 exports.authenticateUser = catchAsync(async (req, res, next) => {
-  const { token } = req.headers;
+  const { headers } = req;
+
+  let token = '';
+
+  if (headers.authorization && headers.authorization.startsWith('Bearer')) {
+    token = headers.authorization.split(' ')[1];
+  }
 
   if (!token) {
     return next(new AppError('Unauthorized access', 401));
   }
 
-  const { id, role } = isTokenValid({ token });
-  req.user = { id, role };
+  const decodedToken = isTokenValid({ token });
 
-  next();
+  const user = await User.findByPk(decodedToken.id);
+
+  if (!user) {
+    return next(new AppError('User no longer exists', 400));
+  }
+
+  req.user = {
+    id: user.id,
+    role: user.role,
+  };
+
+  return next();
 });
