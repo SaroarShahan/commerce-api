dd8b7ae0e3abb09800380e38938e81d0d19e8c42
SaroarShahan <saroar.shahan@gmail.com>
2024-12-30 14:11:19 +0600

    refactor(authController): Add catchAsync and AppError

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -3,89 +3,75 @@ const bcrypt = require('bcrypt');
 const User = require('../db/models/user');
 
 const { generateToken } = require('../utils/jwt');
+const catchAsync = require('../middleware/catchAsync');
+const { AppError } = require('../errors');
 
-exports.signup = async (req, res, next) => {
+exports.signup = catchAsync(async (req, res, next) => {
   const { first_name, last_name, email, password, role } = req.body;
 
-  try {
-    if (!['1', '2'].includes(role)) {
-      return res.status(400).json({
-        status: false,
-        message: 'Invalid role',
-      });
-    }
-
-    const existingUser = await User.findOne({
-      where: {
-        email,
-      },
-    });
-
-    if (existingUser) {
-      return res.status(400).json({
-        status: false,
-        message: 'Email already exists',
-      });
-    }
-
-    const hashPassword = await bcrypt.hash(password, 10);
-
-    const newUser = await User.create({
-      first_name,
-      last_name,
-      email,
-      password: hashPassword,
-      role,
-    });
-
-    delete newUser.dataValues.password;
-    delete newUser.dataValues.deletedAt;
-
-    res.status(201).json({
-      status: true,
-      message: 'User signed up successfully',
-      data: newUser,
-    });
-  } catch (error) {
-    next(error);
+  if (!['1', '2'].includes(role)) {
+    throw new AppError('Invalid role', 400);
   }
-};
 
-exports.login = async (req, res, next) => {
+  const existingUser = await User.findOne({
+    where: {
+      email,
+    },
+  });
+
+  if (existingUser) {
+    throw new AppError('Email already exists', 400);
+  }
+
+  const hashPassword = await bcrypt.hash(password, 10);
+
+  const newUser = await User.create({
+    first_name,
+    last_name,
+    email,
+    password: hashPassword,
+    role,
+  });
+
+  if (!newUser) {
+    return next(new AppError('Failed to create the user', 400));
+  }
+
+  delete newUser.dataValues.password;
+  delete newUser.dataValues.deletedAt;
+
+  res.status(201).json({
+    status: true,
+    message: 'User signed up successfully',
+    data: newUser,
+  });
+});
+
+exports.login = catchAsync(async (req, res, next) => {
   const { email, password } = req.body;
 
-  try {
-    if (!email || !password) {
-      return res.status(400).json({
-        status: false,
-        message: 'Email and password are required',
-      });
-    }
-
-    const user = await User.findOne({
-      where: {
-        email,
-      },
-    });
-
-    if (!user || !(await bcrypt.compare(password, user.dataValues.password))) {
-      return res.status(401).json({
-        status: false,
-        message: 'Invalid email or password',
-      });
-    }
-
-    const token = await generateToken({
-      id: user.dataValues.id,
-      role: user.dataValues.role,
-    });
-
-    res.status(200).json({
-      status: true,
-      message: 'User logged in successfully',
-      data: { token },
-    });
-  } catch (error) {
-    next(error);
+  if (!email || !password) {
+    return next(new AppError('Email and password are required', 400));
   }
-};
+
+  const user = await User.findOne({
+    where: {
+      email,
+    },
+  });
+
+  if (!user || !(await bcrypt.compare(password, user.dataValues.password))) {
+    return next(new AppError('Invalid email or password', 401));
+  }
+
+  const token = await generateToken({
+    id: user.dataValues.id,
+    role: user.dataValues.role,
+  });
+
+  res.status(200).json({
+    status: true,
+    message: 'User logged in successfully',
+    data: { token },
+  });
+});
