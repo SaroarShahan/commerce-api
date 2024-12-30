96f9cab6a613729c593a0fa46d7c12b479305b15
SaroarShahan <saroar.shahan@gmail.com>
2024-12-30 16:24:27 +0600

    feat(isTokenValid): Add isTokenValid utils

--- src/utils/jwt.js
+++ src/utils/jwt.js
@@ -3,3 +3,5 @@ const jwt = require('jsonwebtoken');
 exports.generateToken = async (payload) => {
   return jwt.sign(payload, process.env.JWT_SECRET_KEY, { expiresIn: process.env.JWT_EXPIRES_IN });
 };
+
+exports.isTokenValid = ({ token }) => jwt.verify(token, process.env.JWT_SECRET_KEY);
